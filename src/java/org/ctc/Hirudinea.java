1d21d12bc92a3f73b0b9c1c8a09b11aadae6d779
gcleenew <cleenewerckguillaume@gmail.com>
2015-04-17 15:27:47 +0200

    Debug, writting the  newStationEntry method to add an entry to the HashMap

--- src/java/org/ctc/Hirudinea.java
+++ src/java/org/ctc/Hirudinea.java
@@ -1,14 +1,24 @@
-
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.Iterator;
 
-
-public static class Hirudinea
+public class Hirudinea
 {
-	private static final ConcurrentHashMap<String, Array> stations = new ConcurrentHashMap();
+	private static final ConcurrentHashMap<String, ArrayList> stations = new ConcurrentHashMap();
 
-	public void newStationEntry(String station, Array entry) {
-        Hirudinea.stations.putIfAbsent(station, entry);
+	public static void newStationEntry(String nom_station, ArrayList entry) {
+		if (stations.get(nom_station) == null) {
+			ArrayList<ArrayList> info_station = new ArrayList<ArrayList>();
+			info_station = stations.get(nom_station);
+			info_station.add(entry);
+			stations.put(nom_station, info_station);
+		}
+		else {
+			ArrayList<ArrayList> info_station = new ArrayList<ArrayList>();
+			info_station.add(entry);
+			stations.putIfAbsent(nom_station, info_station);
+		}
     }
 
 
